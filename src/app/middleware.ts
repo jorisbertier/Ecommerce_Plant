1dfe035bbd1c56818e7ede0e72ccbfc636d99129
Joris Bertier <joris.bertier@gmail.com>
2025-02-10 11:23:22 -0500

    remove commentary

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -11,8 +11,6 @@ export async function middleware(req: NextRequest) {
 
     return NextResponse.next();
 }
-//comment
-
 //export
 export const config = {
   matcher: ["/dashboard/:path*", "/admin/:path*"],
